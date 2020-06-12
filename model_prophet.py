ad7f2f92bf9f640da1731f62f5466da257a6c9bd
Emily Voytek <emily.voytek@gmail.com>
2020-06-12 13:07:07 -0400

    Clean up Prophet testing model

--- model_prophet.py
+++ model_prophet.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-source activate insight
-
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly
 import fbprophet as Prophet
 
 
+plt.rcParams['font.size']=14
 
 
 dbname = 'melbourne_db'
@@ -27,71 +25,43 @@ print('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
 # Sample SQL query
 sql_query = """SELECT * FROM data_ped_stations WHERE sensor_id < '10';"""
 
-pd.read_sql_query(sql_query,engine)
-
-
-sql_query = """
-SELECT date_time, hourly_counts FROM data_ped_historic WHERE sensor_id = '43' AND year <'2020' AND year >'1990';
-"""
-
 df = pd.read_sql_query(sql_query,engine)
 
 
+
+
+### Prepping for Prophet
+sql_query = """SELECT date_time, hourly_counts FROM data_ped_historic WHERE sensor_id = '43' AND year <'2020' AND year >'1990';"""
+df = pd.read_sql_query(sql_query,engine)
 df.date_time.min()
-
-
 df.columns= ['ds','y']
-
 df['y'] = df['y'].replace(',','', regex=True).astype(float)
 
-
-
-
-
-import fbprophet
-m = fbprophet.Prophet()
-
-
-
-
+### Running Prophet
+m = Prophet.Prophet()
 m.fit(df)
-
-
-
-future = m.make_future_dataframe(freq='H',periods=100)
-future
-
-
-# In[12]:
-
-
+future = m.make_future_dataframe(freq='H',periods=2400)
 forecast = m.predict(future)
 
+### Plotting Prophet
+m.plot(forecast);
+fig = m.plot_components(forecast);
+
+
+fig.savefig('prophet2.png',transparent=True,dpi=400)
 
 
 
 
+def default_Prophet_plot(arg):
 
+    figsize=(10,6)
+    xlabel='ds'
+    ylabel='y'
 
-fig = plt.figure(facecolor='w',figsize=figsize)
+    fig = plt.figure(facecolor='w',figsize=figsize)
 
-
-
-
-
-figsize=(10,6)
-xlabel='ds'
-ylabel='y'
-
-fig = plt.figure(facecolor='w',figsize=figsize)
-
-ax = fig.add_suplot(111)
-
-
-# In[ ]:
-
-
-
+    ax = fig.add_suplot(111)
 
     ax.plot(fcst_t, fcst['yhat'], ls='-', c='#0072B2')
     if 'cap' in fcst and plot_cap:
@@ -108,27 +78,6 @@ ax = fig.add_suplot(111)
     return fig
 
 
-# In[ ]:
-
-
-m.plot_components(forecast)
-
-
-# In[ ]:
-
-
-m.plot_components(forecast)
-
-
-# In[ ]:
-
-
-# I save my iPython scripts as pure python scripts for versioning in git.
-# They are much cleaner.
-get_ipython().system('jupyter nbconvert --to=python Processing_timelapse.ipynb')
-
-
-# In[ ]:
 
 
 
@@ -136,16 +85,41 @@ get_ipython().system('jupyter nbconvert --to=python Processing_timelapse.ipynb')
 
 
 
-# SARIMAX example
-from statsmodels.tsa.statespace.sarimax import SARIMAX
-from random import random
-# contrived dataset
-data1 = [x + random() for x in range(1, 100)]
-data2 = [x + random() for x in range(101, 200)]
-# fit model
-model = SARIMAX(data1, exog=data2, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0))
-model_fit = model.fit(disp=False)
-# make prediction
-exog2 = [200 + random()]
-yhat = model_fit.predict(len(data1), len(data1), exog=[exog2])
-print(yhat)
+
+## This might be helpful in extracting frequency decomposition from Prophet model
+days = (pd.date_range(start='2019-01-01', periods=365) + pd.Timedelta(days=0))
+df_y = m.seasonality_plot_df(days)
+seas = model.predict_seasonal_components(df_y)
+
+## This one, too.
+def my_custom_plot_weekly(m, ax=None, uncertainty=True, weekly_start=0, figsize=(10, 6), name='weekly'):
+    """Plot the weekly component of the forecast.
+    Parameters
+    ----------
+    m: Prophet model.
+    ax: Optional matplotlib Axes to plot on. One will be created if this
+        is not provided.
+    uncertainty: Optional boolean to plot uncertainty intervals, which will
+        only be done if m.uncertainty_samples > 0.
+    weekly_start: Optional int specifying the start day of the weekly
+        seasonality plot. 0 (default) starts the week on Sunday. 1 shifts
+        by 1 day to Monday, and so on.
+    figsize: Optional tuple width, height in inches.
+    name: Name of seasonality component if changed from default 'weekly'.
+    Returns
+    -------
+    a list of matplotlib artists
+    """
+    artists = []
+    if not ax:
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111)
+    # Compute weekly seasonality for a Sun-Sat sequence of dates.
+    days = (pd.date_range(start='2017-01-01', periods=7) +
+            pd.Timedelta(days=weekly_start))
+    # Import this function: seasonality_plot_df
+    df_w = seasonality_plot_df(m, days)
+    seas = m.predict_seasonal_components(df_w)
+    days = days.weekday_name
+    # Return the data here, do not plot.
+    return days, seas
