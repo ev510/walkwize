283cd5c2f7172698e9c9eb447c1ae5a4a333ea98
Emily Voytek <emily.voytek@gmail.com>
2020-06-12 05:48:28 -0400

    concat modeled_ped_data with ped_station lat long works

--- walkwize.py
+++ walkwize.py
@@ -64,7 +64,22 @@ def make_future():
     y_future, X_future = dmatrices(expr, future, return_type='dataframe')
     return y_future, X_future
 
+def get_ped_predicted():
+	[df_test, df_train, poisson_training_results, nb2_training_results,y_train,y_test,X_train,X_test] = pickle.load( open( "save.p", "rb" ) )#
+	station_IDs = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 17, 18, 19, 20, 21,
+	            22, 23, 24, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 42, 43,
+	            44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
 
+	y_future, X_future = make_future()
+
+	for SID in station_IDs:
+	 	y_future.insert(1,SID, poisson_training_results[SID].get_prediction(X_future).summary_frame()['mean'], True)
+
+	y_future= y_future.drop('hourly_counts',axis=1)
+	ped_predicted = y_future.transpose()
+	type(ped_predicted.index)
+	return ped_predicted
+	#ped_predicted.index.is_numeric()
 
 
 ########
@@ -114,6 +129,7 @@ def get_ped_data_current():
 	ped_current = pd.read_json("https://data.melbourne.vic.gov.au/resource/d6mv-s43h.json")
 	ped_current = ped_current.groupby('sensor_id')['total_of_directions'].sum().to_frame()
 	ped_current = ped_current.join(ped_stations[['latitude','longitude']])
+
 
 	return ped_current
 
@@ -272,22 +288,19 @@ def source_to_dest(G, gdf_nodes, gdf_edges, s, e):
 # Then implement model based current trends (a different model?)
 
 #import model parameters
-[df_test, df_train, poisson_training_results, nb2_training_results,y_train,y_test,X_train,X_test] = pickle.load( open( "save.p", "rb" ) )#
-station_IDs = [ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 17, 18, 19, 20, 21,
-            22, 23, 24, 26, 27, 28, 29, 30, 31, 34, 35, 36, 37, 40, 41, 42, 43,
-            44, 45, 46, 47, 48, 49, 50, 51, 52, 53]
-
-y_future, X_future = make_future()
- #y_future = y_future.to_frame()
- type(y_future)
-for SID in station_IDs:
- 	y_future.insert(1,str(SID), poisson_training_results[SID].get_prediction(X_future).summary_frame()['mean'], True)
-
-y_future
 
 G, gdf_nodes, gdf_edges= get_map_data()
 ped_stations = get_ped_station_data()
 ped_current = get_ped_data_current()
+ped_predicted = get_ped_predicted()
+ped_stations.index.name
+ped_predicted.index.name = 'sensor_id'
+type(ped_stations.index)
+
+
+pd.concat([ped_predicted,ped_stations[['latitude','longitude']]],axis=1, join="inner")
+ped_predicted.concat(ped_stations[['latitude','longitude']])
+ped_predicted
 
 st.sidebar.title("WalkWize");
 st.sidebar.markdown("*Take the path least traveled*");
@@ -298,7 +311,10 @@ input2 = st.sidebar.text_input('Where are you going?');
 #date = st.sidebar.date_input('When you you want to leave?',  max_value=dt.datetime(2020, 12, 31, 0, 0));
 #time = st.sidebar.time_input('What time do you want to leave?', value=None, key=None);
 
+# gdf_edges['ped_rate'] = interpolate.griddata(np.array(tuple(zip(ped_current['latitude'], ped_current['longitude']))),np.ones_like(np.array(ped_current['total_of_directions'])),np.array(tuple(zip(gdf_edges['centroid_y'], gdf_edges['centroid_x']))), method='cubic',rescale=False,fill_value=0)
+
 gdf_edges['ped_rate'] = interpolate.griddata(np.array(tuple(zip(ped_current['latitude'], ped_current['longitude']))),np.ones_like(np.array(ped_current['total_of_directions'])),np.array(tuple(zip(gdf_edges['centroid_y'], gdf_edges['centroid_x']))), method='cubic',rescale=False,fill_value=0)
+
 # COLOR_BREWER_RED is not activated, default color range is used
 COLOR_BREWER_RED = [[255,247,236],[127,0,0]]
 ped_layer = make_pedlayer(gdf_edges[['centroid_x','centroid_y','ped_rate']],COLOR_BREWER_RED)
