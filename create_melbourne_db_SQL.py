90a2effba9c80665b73cfc6d93a3151680ab35b8
Emily Voytek <emily.voytek@gmail.com>
2020-06-12 12:54:04 -0400

    Clean up.

--- create_melbourne_db_SQL.py
+++ create_melbourne_db_SQL.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# This Notebook documents the creation of SQL database melbourne_db.
-
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import psycopg2
@@ -13,32 +8,32 @@ dbname = 'melbourne_db'
 username = 'emilyvoytek'
 pswd = 'founded1835'
 
-## 'engine' is a connection to a database
+###### 'engine' is a connection to a database
 engine = create_engine('postgresql://%s:%s@localhost/%s'%(username,pswd,dbname))
 
 
-# LOAD THE HISTORIC PED DATA
+######  LOAD THE HISTORIC PED DATA  ######
 ped_historic = pd.read_csv('./data/Pedestrian_Counting_System___2009_to_Present__counts_per_hour_.csv')
 ped_historic.columns=(['id', 'date_time', 'year', 'month', 'mdate', 'day', 'time', 'sensor_id',
        'sensor_name', 'hourly_counts'])
 ped_historic['hourly_counts'] = ped_historic['hourly_counts'].replace(',','', regex=True).astype(float)
-ped_historic
+
 
 # ATTENTION: THIS TAKES FOREVER (~5-10 min) TO RUN
 ped_historic.to_sql('data_ped_historic', engine, if_exists='replace')
 
 
-# LOAD THE STATION INFORMATION
+######  LOAD PED STATION INFORMATION  ######
 ped_stations = pd.read_csv('./data/Pedestrian_Counting_System_Sensor_Locations.csv')
 ped_stations['installation_date'] = pd.to_datetime(ped_stations['installation_date'])
 
 ped_stations.set_index("sensor_id",inplace=True)
 ped_stations = ped_stations.sort_index()
 ped_stations.to_sql('data_ped_stations', engine, if_exists='replace')
-ped_stations
 
 
-# LOAD HISTORIC WEATHER DATA
+
+###### LOAD HISTORIC WEATHER DATA ######
 df_rain = pd.read_csv('./data/IDCJAC0009_086338_1800_Data.csv')
 df_rain.columns = ['product_code', 'station_number', 'year', 'month', 'day', 'rainfall_mm','days_measured', 'quality']
 df_rain['datetime']=pd.to_datetime(df_rain[['year','month', 'day']])
@@ -64,22 +59,16 @@ df_solar.columns = ['product_code', 'station_Number', 'year', 'month', 'day', 'd
 df_solar['datetime']=pd.to_datetime(df_solar[['year','month', 'day']])
 df_solar = df_solar.set_index(pd.to_datetime(df_solar['datetime']))
 #df_solar.to_sql('data_solar', engine, if_exists='replace')
-
-
-
+#
+# COMBINED WEATHER DB
 df_weather = pd.concat([df_temp_min['minimum_temperature_C'],df_temp_max['maximum_temperature_C'],df_solar['daily_solar_exposure_MJ'], df_rain['rainfall_mm']], axis=1, join='inner')
+df_weather = df_weather.resample('1H').pad()
 df_weather.to_sql('data_weather', engine, if_exists='replace')
 
 
-######
 
 
-
-
-
-
-###### EDA #######
-
+###### Identify stations to model #######
 def identify_useable_stations(ped_stations):
     # Active stations, installed before Jan 01, 2018
     ped_stations_to_use = ped_stations[(ped_stations.installation_date<dt.datetime(2018,1,1,0,0,0)) & (ped_stations['status']=='A')].index
@@ -87,8 +76,10 @@ def identify_useable_stations(ped_stations):
 
 index = identify_useable_stations(ped_stations)
 
-
-####
-
 ped_historic.groupby(by='sensor_id')['date_time'].agg(["min","max"])
-ped_historic.groupby(by='sensor_id')['hourly_counts'].agg(["min","max","mean"])
+
+
+
+
+
+###### EDA #######
